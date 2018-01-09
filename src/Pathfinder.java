d4dea854f6cc0b6f80f47ad6e4ef29b15019cfa3
Dylan Russell <dylanrussell@Dylans-MacBook-Pro.local>
2018-01-08 19:29:55 -1000

    change

--- src/Pathfinder.java
+++ src/Pathfinder.java
@@ -3,7 +3,7 @@ public class Pathfinder {
 	
 	private void addToPath(MapLocation mapLocation) {
 		int[] point = {mapLocation.getX(), mapLocation.getY()};
-		Grid.PATHPOINT.add( point);
+		Grid.PATHPOINT.add(point);
 	}
 	
 }
